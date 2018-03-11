b28f71a21925b26fb2616eec4f79f55b5245037a
Igor Ivannikov <screamolet@gmail.com>
2018-03-11 08:39:18 +0200

    smth

---
+++ src/Hw4Square.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Hw4Square {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("¬ведите сторону - а");
+		int a = sc.nextInt();
+		System.out.println("¬ведите сторону - b");
+		int b = sc.nextInt();
+
+		String[][] sqr = new String[a][b];
+		for (int i = 0; i < a; i++) {
+			for (int j = 0; j < b; j++) {
+				sqr[i][j] = "+";
+//			}
+//		}
+//		for (int i = 1; i < a - 1; i++) {
+//			for (int j = 1; j < b - 1; j++) {
+//				sqr[i][j] = " ";
+//			}
+//		}
+//		for (int i = 0; i < a; i++) {
+//			for (int j = 0; j < b; j++) {
+				System.out.print(sqr[i][j]);
+			}
+			System.out.println();
+			sc.close();
+		}
+	}
+}
